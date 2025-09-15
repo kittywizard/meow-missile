3193f7cb76d560dc67a8d75c3fadc8cf5b5a6c13
kittywizard <volcana@gmail.com>
2025-09-15 14:44:41 -0700

    basic visual for character select

--- src/game/scenes/CharacterSelect.ts
+++ src/game/scenes/CharacterSelect.ts
@@ -13,21 +13,26 @@ export default class CharacterSelect extends Phaser.Scene {
 
     showCharacters() {
         this.background = this.add.tileSprite(0, 0, this.width, this.height, "background").setOrigin(0).setScrollFactor(0, 1);
-/*
-        this.scene.anims.create({
-            key: this.name,
-            frames: this.scene.anims.generateFrameNumbers(this.name, { start: 0, end: 0 }),
-            frameRate: 10,
-            repeat: -1
+
+        //TO DO
+            // need to set a variable that will set sprite / character info. to start probably just swap sprites
+            // will adding these to a group make it easier to code?
+            // keyboard controls too
+         const tali = this.add.sprite(this.center_width, this.center_height - 40, 'tali-select').setInteractive();
+        
+        let activeState: boolean = false;
+        tali.on('pointerdown', () => {
+            if(!activeState) {
+               // tali.setTint(0x5564d9);
+                tali.setFrame(1);
+                activeState = !activeState;
+            } else {
+                //tali.clearTint();
+                tali.setFrame(0);
+                activeState = !activeState;
+            }
         });
-        this.scene.anims.create({
-            key: this.name + "selected",
-            frames: this.scene.anims.generateFrameNumbers(this.name, { start: 1, end: 1 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.play(this.name, true);
-*/
+
         this.add.bitmapText(this.center_width, this.center_height + 100, 'minogram', 'Select a Character', 30).setOrigin(0.5).setTintFill(0x000000);
         this.add.bitmapText(this.center_width, this.center_height + 140, 'minogram', 'SPACE to start', 30).setOrigin(0.5).setTintFill(0x000000);
 
